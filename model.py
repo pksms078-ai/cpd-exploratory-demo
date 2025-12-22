431480f8afdf8e05be7bccbf99a67f120b88441b
Prabin Kumar <pksms078@gmail.com>
2025-12-22 17:25:30 +0530

    ProteinSequenceModel for scoring protein sequences
    Implemented a lightweight machine learning model for scoring protein sequences using a RandomForestRegressor.

--- model.py
+++ model.py
@@ -1,1 +1,40 @@
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from .utils import encode_sequence
+
+class ProteinSequenceModel:
+    """
+    Lightweight ML model for exploratory
+    protein sequence scoring.
+
+    This model is NOT a biological oracle.
+    It reduces search space for experiments.
+    """
+
+    def __init__(self):
+        self.model = RandomForestRegressor(
+            n_estimators=50,
+            random_state=42
+        )
+        self.trained = False
+
+    def fit(self, sequences, scores):
+        """
+        Train model on sequence-score pairs.
+        """
+        X = np.array([encode_sequence(s) for s in sequences])
+        y = np.array(scores)
+
+        self.model.fit(X, y)
+        self.trained = True
+
+    def predict(self, sequences):
+        """
+        Predict relative scores for new sequences.
+        """
+        if not self.trained:
+            raise RuntimeError("Model not trained")
+
+        X = np.array([encode_sequence(s) for s in sequences])
+        return self.model.predict(X)
 
