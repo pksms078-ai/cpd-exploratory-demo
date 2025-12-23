c9e233fadab17b9861bc6e75cc03e8701dd9739d
Prabin Kumar <pksms078@gmail.com>
2025-12-23 20:27:18 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="cpd-exploratory-demo",
     version="0.1.0",
-    description="Computational Protein Design – Exploratory Demo",
-    author="Prabin Kumar",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=["numpy"],
