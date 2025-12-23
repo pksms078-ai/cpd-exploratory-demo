4065a093fef30205635afaf82e980ed68f105f86
Prabin Kumar <pksms078@gmail.com>
2025-12-23 20:55:58 +0530

    model.py

--- model.py
+++ model.py
@@ -1,2 +1,19 @@
-def dummy_model(x):
-    return x * 2
+"""
+Model module for CPD Exploratory Demo
+"""
+
+def dummy_model(x: int) -> int:
+    """
+    Simple dummy model for validation & benchmarking.
+
+    Parameters
+    ----------
+    x : int
+        Input integer
+
+    Returns
+    -------
+    int
+        Squared value
+    """
+    return x * x
