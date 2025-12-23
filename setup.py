c17df3e8937a2bb3ab0f52ceb44b15aa14bac85c
Prabin Kumar <pksms078@gmail.com>
2025-12-23 17:02:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cpd-exploratory-demo",
+    name="cpd-exploratory",
     version="0.1.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=[],
 )
