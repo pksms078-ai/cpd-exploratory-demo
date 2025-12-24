2ab20144406c38ed64fef41efc228d653d9d0e12
Prabin Kumar <pksms078@gmail.com>
2025-12-24 11:22:51 +0530

    model.py

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
+# src/cpd_exploratory/model.py
+
 def dummy_model(x: int) -> int:
     """
     Dummy model for benchmarking and pipeline validation.
     """
     return x * x
+
+
+if __name__ == "__main__":
+    print(dummy_model(5))
+
