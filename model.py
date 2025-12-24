f1ce03718e7573ab051b6b56000d8936b3611181
Prabin Kumar <pksms078@gmail.com>
2025-12-24 19:20:08 +0530

    model.py

--- model.py
+++ model.py
@@ -1,21 +1,4 @@
-cat > src/cpd_exploratory/model.py << 'EOF'
+cat << 'EOF' > src/cpd_exploratory/model.py
 def dummy_model(x: int) -> int:
-    """
-    Dummy_model for benchmarking and pipeline validation.
-    """
     return x * x
-if __name__ == "__main__":
-    print(dummy_model(5))
 EOF
-
-cat > src/cpd_exploratory/model.py << 'EOF'
-def dummy_model(x: int) -> int:
-    """
-    Dummy model for benchmarking and pipeline validation.
-    """
-    return x * x
-if __name__ == "__main__":
-    print(dummy_model(5))
-EOF
-def dummy_model(x):
-    return x * 2
